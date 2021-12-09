cf38746d05679743e080b72075669f224b9908e4
Lovro2 <95843165+Lovro2@users.noreply.github.com>
2021-12-09 08:39:03 +0100

    Add files via upload

---
+++ Lovro.duric1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+int stranicaA=8;
+int stranicaB=10;
+int stranicaC=12;
+printf("Unesi;str\n");
+scanf("%d %d %d",&stranicaA,&stranicaB,&stranicaC);
+int volumen=stranicaA*stranicaB*stranicaC;
+printf("%d",volumen);
+}
+
