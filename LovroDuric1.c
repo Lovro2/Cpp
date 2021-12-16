2448c1ba2a9d4a73c1c876e353307307214eb410
Lovro2 <95843165+Lovro2@users.noreply.github.com>
2021-12-16 09:33:34 +0100

    Add files via upload

---
+++ LovroDuric1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a=3;
+    int b=5;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    int P= (a*b)/2;
+
+    printf ("površina trokuta je = %d\n",P);
+
+    return 0;
+}
+
+
