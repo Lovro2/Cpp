80f2947125e510553d2e143b0a30c31ab0211997
Lovro2 <95843165+Lovro2@users.noreply.github.com>
2022-02-03 07:40:14 +0100

    Add files via upload

---
+++ novci.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+int main()
+
+{
+    int a;
+
+    printf("Unesi ukupnu zaradu\n");
+    scanf("%d",&a);
+
+    int x=(((a*4/5)*4/5)*4/5);
+
+    printf("njemu je ostalo %d\n",x);
+
+    return 0;
+}
+
