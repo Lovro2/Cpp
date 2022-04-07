b71d610270255b4c94a12abb538aa2f6f49117f7
Lovro2 <95843165+Lovro2@users.noreply.github.com>
2022-04-07 07:42:14 +0200

    Add files via upload

--- Untitled1.c
+++ Untitled1.c
@@ -1,17 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main()
-
+    int main()
 {
-    int sum=0, i=10, n;
 
-    while(i){
-        scanf("%d", &n);
-        sum+=n;
-        i--;
+    int n,m=0,sum=0;
+    printf("Unesi broj\n");
+    scanf("%d",&n);
+
+    while (n){
+    m=m*10+n%10;
+    n/=10;
     }
-    printf("ukupno:%d",sum);
-    printf("prosjecno:%f",(float)sum/10);
-}
+
+    while (m){
+    scanf("%d",&n);
+    sum+=n*(m%10);
+    m/=10;
+    }
+    printf("%d \n",sum);
+
+    return 0;
+    }
