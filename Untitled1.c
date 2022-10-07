b4cc04ee27a7096fb9eff1cac6cf21453e61655f
Lovro2 <95843165+Lovro2@users.noreply.github.com>
2022-10-07 10:14:40 +0200

    Add files via upload

--- Untitled1.c
+++ Untitled1.c
@@ -1,28 +1,14 @@
-#include<stdlib.h>
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+ int moze(int x){
+        return(x+7)>170;
+    }
 
 int main()
-
 {
-    int n,k,A1,A2,A3,A4,A5,B,brBod=0;
-    printf("Unesi broj zadataka i koliko Josip zna\n");
-    scanf("%d", &n);
-    printf("unesi broj zadataka\n");
-    scanf("%d", &k);
-    printf("unesi koliko zadataka Josip zna\n");
-    scanf("%d %d %d %d %d", &A1, &A2, &A3, &A4, &A5);
-
-    while(k){
-            k--;
-    scanf("%d",B);
-
-    if(B==A1||B==A2||B==A3||B==A4||B==A5) brBod++;
-    }
-    brBod=brBod==0?1:brBod;
-
-    printf("Dobio je %d",brBod);
-
-
-
-
+    int x;
+    scanf("%d",&x);
+    printf(moze(x)?"moze":"ne moze");
 }
