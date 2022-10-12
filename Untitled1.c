682ecc114ecf14a512ad243936c2f06fe04bda1c
Lovro2 <95843165+Lovro2@users.noreply.github.com>
2022-10-12 10:26:42 +0200

    Add files via upload

--- Untitled1.c
+++ Untitled1.c
@@ -1,22 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+int nazivnik(n){
+return n==1?1:n+nazivnik(--n)+1;
 
-int Dominik(int x, int y, int z){
-if(z>x)
-return printf("1");
-if(z>=x&&z>(x+y))
-return printf("2");
-if(if(z>=x&&z<(x+y));
-return printf("0");
+}
+float funkcija(x,n){
+
+return n==1?x:pow(x,n);
+
 }
 
 int main()
 {
-    int a,b,c;
-    printf("Unesi cijenu teretane mjesecno\n");
-    scanf("%d", &a);
-    printf("Unesi cijenu privatnog trenera\n");
-    scanf("%d", &b);
-    printf("Unesi Dominikov ukupni budzet\n");
-    scanf("%d", &c);
+   int x;
+   int n;
+   float a;
+   printf("unesi neki broj\n");
+   scanf("%d",&x);
+   printf("unesi neki broj\n");
+   scanf("%d",&n);
+    a=funkcija(x,n)/nazivnik(n);
+    printf("%f",a);
+
 }
